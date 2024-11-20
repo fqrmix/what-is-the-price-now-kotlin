4e8b4df5fe58648c7a4b84dd8423ccd4d035f18c
Семен В. Сергеев <svsergeev@yoomoney.ru>
2024-11-20 16:37:45 +0300

    Changed checkPriceChange method

--- src/main/kotlin/service/ArticleService.kt
+++ src/main/kotlin/service/ArticleService.kt
@@ -34,7 +34,7 @@ class ArticleService {
     suspend fun checkPriceChange(article: Article): Pair<BigDecimal?, Boolean> {
         return withContext(Dispatchers.IO) {
             val newPrice = CheckArticlePriceCommand().execute(article)
-             if (article.price > newPrice) {
+             if (article.price != newPrice) {
                 Pair(newPrice, true) // Цена изменилась
             } else {
                 Pair(null, false) // Цена осталась той же
